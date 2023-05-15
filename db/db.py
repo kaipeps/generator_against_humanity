14876332364da59cd201d1110dac36a881748523
kaipeps <kaipepper99@gmail.com>
2023-05-15 18:50:11 +0930

    db_url updated for Render, minor CSS

--- db/db.py
+++ db/db.py
@@ -1,7 +1,7 @@
 import psycopg2
 import psycopg2.extras
 
-DB_URL = 'dbname=gah_db'
+DB_URL = 'postgres://gah_db_user:ULeYl2ZDZ9yHDij2Dda1YItLLZ16EYbO@dpg-chgun2bhp8u065rbmv40-a/gah_db'
 
 def sql(query, parameters=[]):
     connection = psycopg2.connect(DB_URL)
